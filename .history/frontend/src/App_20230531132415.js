beb4d8092b2178d6872d0e31f935935f241289c6
Mika Steyn <104835305+mikasteyn999@users.noreply.github.com>
2023-06-09 20:19:21 +0200

    Delete .history directory

--- .history/frontend/src/App_20230531132415.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Router from './Router';
-
-// Updating the App component to use the Router component
-function App() {
-  return <Router />;
-}
-
-export default App;
